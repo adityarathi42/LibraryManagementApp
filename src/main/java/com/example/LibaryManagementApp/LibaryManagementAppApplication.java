bcf0c4007ae5d5e535ac758eab584ae7a55ddcfd
adityarathi42 <adityarathi42@gmail.com>
2023-01-17 15:55:51 +0530

    initialize

---
+++ src/main/java/com/example/LibaryManagementApp/LibaryManagementAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.LibaryManagementApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibaryManagementAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibaryManagementAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/LibaryManagementApp/LibaryManagementAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LibaryManagementApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibaryManagementAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
